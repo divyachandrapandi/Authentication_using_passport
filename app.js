9ae99c010ebd268fe3c25f4bf47c6a9997ae588d
divyachandrapandi <divya4shivalaya@gmail.com>
2022-07-30 20:33:04 +0530

    LEVEL - 4 SALTING

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-require('dotenv').config();
+require('dotenv').config(); // ENVIRONMENT VARIABLE
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require ('mongoose-encryption');
+const bcrypt = require('bcrypt'); // FOR LEVEL -4 SALTING 
+const saltRounds = 10;
+// const md5 = require('md5');  // FOR LEVEL-3 HASHING
+// const encrypt = require ('mongoose-encryption');  // FOR LEVEL 2 ENCRYPTION
 
 // -----------------------------EXPRESS SETUP ---------------------------------//
 
 const app = express();
-
-
 
 // -----------------------------STATIC SETUP ---------------------------------//
 
@@ -21,22 +22,31 @@ app.set('view engine', 'ejs');
 
 // -----------------------------BODY-PARSER SETUP ---------------------------------//
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}));
 
+// -----------------------------CONNECT TO DB ---------------------------------//
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser :true});
 
+// -----------------------------CREATE TO SCHEMA---------------------------------//
 
 const userSchema = new mongoose.Schema({
     email:String,
     password:String
 });
 
+// -----------------------------SECRET KEY FOR ENCRYPTION ---------------------------------//
+
 const secret = process.env.SECRET_KEY;
 
-userSchema.plugin(encrypt, {secret : secret, encryptedFields: ['password']});
+// -----------------------------PLUGIN TO SCHEMA LEVEL -2 ---------------------------------//
+
+// userSchema.plugin(encrypt, {secret : secret, encryptedFields : ['password']});
+
+// -----------------------------PLUGIN TO SCHEMA ---------------------------------//
 
 const User = mongoose.model('User', userSchema);
+
 // -----------------------------HOME ROUTE ---------------------------------//
 
 app.get("/", function(req,res){
@@ -50,18 +60,22 @@ app.get("/login", function(req,res){
 });
 
 // -----------------------------POST LOGIN ROUTE ---------------------------------//
+
 app.post("/login", function(req,res){
     const username = req.body.username;
     const password = req.body.password;
+    // const password = md5(req.body.password);  
     
     User.findOne({email:username}, function(err, foundUser){
         if (err){
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password){
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if (result === true){
+                        res.render("secrets");
+                    }                    
+                });
             }
         }
     });
@@ -77,21 +91,25 @@ app.get("/register", function(req,res){
 
 app.post("/register", function(req,res){
 
-    const newUser = new User ({
-        email:req.body.username,
-        password:req.body.password
-    });
-
-    newUser.save(function(err){
-        if(!err){
-            res.render("secrets")
-        } else {
-            console.log(err);
-        }
-    });
-})
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User ({
+            email:req.body.username,
+            password: hash
+        });
+        // console.log(newUser.password)
+        newUser.save(function(err){
+            if(!err){
+                res.render("secrets")
+            } else {
+                console.log(err);
+            }
+        });
+    });    
+});
+//      LEVEL- 3 HASH USE password: md5(req.body.password)
 // -----------------------------SERVER LISTENING---------------------------------//
 
 app.listen(5000, function(){
     console.log("Server running on port 5000");
 });
+
